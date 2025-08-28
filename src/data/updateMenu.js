9e69c88622457717bf5ad1f3e32a4d8d08255135
dtluongg <fastdominicracing@gmail.com>
2025-08-28 20:40:55 +0700

    Update 218 image

--- src/data/updateMenu.js
+++ src/data/updateMenu.js
@@ -117,7 +117,7 @@ import image165 from '../assets/menu/165.jpg';
 import image170_181 from '../assets/menu/170_181.jpg';
 import image182_184 from '../assets/menu/182_184.jpg';
 import image185_204 from '../assets/menu/185_204.jpg';
-import image206_216 from '../assets/menu/206_216.jpg';
+import image205_218 from '../assets/menu/205_218.jpg';
 import image230 from '../assets/menu/230.jpg';
 import image231 from '../assets/menu/231.jpg';
 import image232 from '../assets/menu/232.jpg';
@@ -505,17 +505,17 @@ filters: [
         {
           "name": "Spezial warmes Kűches",
           "items": [
-            { "id": "205", "image": image206_216, "name": "Gebratener Lachs Curry", "description": "Gebratener Lachs in Scheiben mit asiat. Gemüse, rotem Curry und, Kokosmilch (mild oder scharf) mit Reis", "price": "15,10 €" },
-            { "id": "206", "image": image206_216, "name": "Gebratener Lachs Chili", "description": "Gebratener Lachs mit asiat. Gemüse, Chili und Pfeffeschorten (sehr scharf)", "price": "15,10 €" },
-            { "id": "207", "image": image206_216, "name": "Gebratener Lachs Mango", "description": "Gebratener Lachs auf Mango-Soße mit Kokosmilch, rotem Curry, frischen Mangostücken, Paprika, Dill und Thai Basilikum.", "price": "15,50 €" },
-            { "id": "208", "image": image206_216, "name": "Mango Ente (mild oder scharf)", "description": "Knusprig gebackene Ente auf Mango - Soße mit Kokosmilch, rotem Curry, frischen Mangostücken, Paprika und Thai Basilikum.", "price": "15,20 €" },
-            { "id": "216a", "image": image206_216, "name": "Gebratene Gemüse mit grünem Curry, Kokosmilch, Reis und : ", "description": "a. Ente Kross", "price": "14,40 €" },
-            { "id": "216b", "image": image206_216, "name": "Gebratene Gemüse mit grünem Curry, Kokosmilch, Reis und : ", "description": "b. Gebackener Hühnerfleisch", "price": "12,60 €" },
-            { "id": "216c", "image": image206_216, "name": "Gebratene Gemüse mit grünem Curry, Kokosmilch, Reis und : ", "description": "c. Rindfleisch", "price": "14,40 €" },
-            { "id": "216d", "image": image206_216, "name": "Gebratene Gemüse mit grünem Curry, Kokosmilch, Reis und : ", "description": "d. Gebratener Lachs", "price": "15,10 €" },
-            { "id": "217a", "image": image206_216, "name": "Gegrillter Lachs mit gem.Salat und Teri Yaki Soße: ", "description": "a. Mit Reis", "price": "14,70 €" },
-            { "id": "217b", "image": image206_216, "name": "Gegrillter Lachs mit gem.Salat und Teri Yaki Soße: ", "description": "b. Süßkartoffel- Pommes", "price": "15,10 €" },
-            { "id": "218", "image": Warme_gerichte, "name": "Gegrilltes Hühnerbrustfilet mit gem.Salat,Reis und Teri Yaki Soße", "description": "", "price": "12,70 €" }
+            { "id": "205", "image": image205_218, "name": "Gebratener Lachs Curry", "description": "Gebratener Lachs in Scheiben mit asiat. Gemüse, rotem Curry und, Kokosmilch (mild oder scharf) mit Reis", "price": "15,10 €" },
+            { "id": "206", "image": image205_218, "name": "Gebratener Lachs Chili", "description": "Gebratener Lachs mit asiat. Gemüse, Chili und Pfeffeschorten (sehr scharf)", "price": "15,10 €" },
+            { "id": "207", "image": image205_218, "name": "Gebratener Lachs Mango", "description": "Gebratener Lachs auf Mango-Soße mit Kokosmilch, rotem Curry, frischen Mangostücken, Paprika, Dill und Thai Basilikum.", "price": "15,50 €" },
+            { "id": "208", "image": image205_218, "name": "Mango Ente (mild oder scharf)", "description": "Knusprig gebackene Ente auf Mango - Soße mit Kokosmilch, rotem Curry, frischen Mangostücken, Paprika und Thai Basilikum.", "price": "15,20 €" },
+            { "id": "216a", "image": image205_218, "name": "Gebratene Gemüse mit grünem Curry, Kokosmilch, Reis und : ", "description": "a. Ente Kross", "price": "14,40 €" },
+            { "id": "216b", "image": image205_218, "name": "Gebratene Gemüse mit grünem Curry, Kokosmilch, Reis und : ", "description": "b. Gebackener Hühnerfleisch", "price": "12,60 €" },
+            { "id": "216c", "image": image205_218, "name": "Gebratene Gemüse mit grünem Curry, Kokosmilch, Reis und : ", "description": "c. Rindfleisch", "price": "14,40 €" },
+            { "id": "216d", "image": image205_218, "name": "Gebratene Gemüse mit grünem Curry, Kokosmilch, Reis und : ", "description": "d. Gebratener Lachs", "price": "15,10 €" },
+            { "id": "217a", "image": image205_218, "name": "Gegrillter Lachs mit gem.Salat und Teri Yaki Soße: ", "description": "a. Mit Reis", "price": "14,70 €" },
+            { "id": "217b", "image": image205_218, "name": "Gegrillter Lachs mit gem.Salat und Teri Yaki Soße: ", "description": "b. Süßkartoffel- Pommes", "price": "15,10 €" },
+            { "id": "218", "image": image205_218, "name": "Gegrilltes Hühnerbrustfilet mit gem.Salat,Reis und Teri Yaki Soße", "description": "", "price": "12,70 €" }
           ]
         }
       ]
